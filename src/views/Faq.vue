a5f19f868580d32b9f87b991847579afb8196cb3
Tristan van de Kamp <tristan@ipublications.net>
2018-09-10 17:52:51 +0200

    faq english

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -7,61 +7,61 @@
         <div class="faq-item">
             <input id="ac-1" type="checkbox" checked />
             <label class="faq-header" id="faq-1" for="ac-1">
-                <h4>Vraag 1</h4>
+                <h4>Question 1</h4>
                 <i class="fa fa-angle-down"></i>
             </label>
             <div class="faq-body">
-                Hier komen dan de antwoorden lalalal test test.
+                This is where the answer is going to be for you to read.
             </div>
         </div>
         <div class="faq-item">
             <input id="ac-2" type="checkbox" />
             <label class="faq-header" id="faq-2" for="ac-2">
-                <h4>Vraag 2</h4>
+                <h4>Question 2</h4>
                 <i class="fa fa-angle-down"></i>
             </label>
             <div class="faq-body">
-                Hier komen dan de antwoorden lalalal test test.
+                This is where the answer is going to be for you to read.
             </div>
         </div>
         <div class="faq-item">
             <input id="ac-3" type="checkbox" />
             <label class="faq-header" id="faq-3" for="ac-3">
-                <h4>Vraag 3</h4>
+                <h4>Question 3</h4>
                 <i class="fa fa-angle-down"></i>
             </label>
             <div class="faq-body">
-                Hier komen dan de antwoorden lalalal test test.
+                This is where the answer is going to be for you to read.
             </div>
         </div>
         <div class="faq-item">
             <input id="ac-4" type="checkbox" />
             <label class="faq-header" id="faq-4" for="ac-4">
-                <h4>Vraag 4</h4>
+                <h4>Question 4</h4>
                 <i class="fa fa-angle-down"></i>
             </label>
             <div class="faq-body">
-                Hier komen dan de antwoorden lalalal test test.
+                This is where the answer is going to be for you to read.
             </div>
         </div>
         <div class="faq-item">
             <input id="ac-5" type="checkbox" />
             <label class="faq-header" id="faq-5" for="ac-5">
-                <h4>Vraag 5</h4>
+                <h4>Question 5</h4>
                 <i class="fa fa-angle-down"></i>
             </label>
             <div class="faq-body">
-                Hier komen dan de antwoorden lalalal test test.
+                This is where the answer is going to be for you to read.
             </div>
         </div>
         <div class="faq-item">
             <input id="ac-6" type="checkbox" />
             <label class="faq-header" id="faq-6" for="ac-6">
-                <h4>Vraag 6</h4>
+                <h4>Question 6</h4>
                 <i class="fa fa-angle-down"></i>
             </label>
             <div class="faq-body">
-                Hier komen dan de antwoorden lalalal test test.
+                This is where the answer is going to be for you to read.
             </div>
         </div>
     </div>
